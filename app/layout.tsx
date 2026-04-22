d597baf27c957a3880410f0e7541c9c8d71fec04
hadyaa552-debug <hadyaa552@gmail.com>
2026-04-22 21:21:04 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Cairo } from "next/font/google"
+import Script from "next/script"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/toaster"
@@ -19,6 +20,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {children}
           <Toaster />
         </ThemeProvider>
+
+        <Script src="https://www.googletagmanager.com/gtag/js?id=AW-18109309038" strategy="afterInteractive" />
+        <Script id="gtag-init" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-18109309038');
+        `}</Script>
       </body>
     </html>
   )
